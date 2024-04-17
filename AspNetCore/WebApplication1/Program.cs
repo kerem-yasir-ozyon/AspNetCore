a6f9bef3686a47b1c18026aeb151f822a3dc9820
kerem-yasir-ozyon <keremyasirozyon@gmail.com>
2024-04-17 17:12:18 +0300

    17.04

--- AspNetCore/WebApplication1/Program.cs
+++ AspNetCore/WebApplication1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
 namespace WebApplication1
 {
     public class Program
@@ -5,11 +8,45 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", async (httpContext) =>
+            //{
+            //    await httpContext.Response.WriteAsync("<a href ='/hakkimizda'>hakkimizda</a><br>");
+            //    await httpContext.Response.WriteAsync("<a href ='/iletisim'>iletisim</a><br>");
+            //    await httpContext.Response.WriteAsync("<a href ='/projeler'>projeler</a><br>");
+            //});
 
-            app.Run();
+
+
+            //app.MapGet("/hakkimizda", async (httpContext) =>
+            //{
+            //    httpContext.Response.ContentType = "text/html; charset=utf-8";
+            //    await httpContext.Response.WriteAsync("<b>hakkimizda sayfamýza hos geldiniz.</b><br>");
+            //    await httpContext.Response.WriteAsync("<a href ='/'>Anasayfa</a><br>");
+
+            //});
+
+            //app.MapGet("/iletisim", async (httpContext) =>
+            //{
+            //    httpContext.Response.ContentType = "text/html; charset=utf-8";
+            //    await httpContext.Response.WriteAsync("<b>iletisim sayfamýza hos geldiniz.</b><br>");
+            //    await httpContext.Response.WriteAsync("<a href ='/'>Anasayfa</a><br>");
+
+            //});
+
+            //app.MapGet("/projeler", async (httpContext) =>
+            //{
+            //    httpContext.Response.ContentType = "text/html; charset=utf-8";
+            //    await httpContext.Response.WriteAsync("<b>Projeler sayfamýza hos geldiniz.</b><br>");
+            //    await httpContext.Response.WriteAsync("<a href ='/'>Anasayfa</a><br>");
+
+            //});
+            app.MapDefaultControllerRoute();
+
+
+            app.Run(); 
         }
     }
 }
