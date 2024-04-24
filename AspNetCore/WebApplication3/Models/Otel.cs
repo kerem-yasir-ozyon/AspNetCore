001aec4f9aee7d3fad72872ddb6cbab8948a61fe
kerem-yasir-ozyon <keremyasirozyon@gmail.com>
2024-04-24 17:08:33 +0300

    24.042

---
+++ AspNetCore/WebApplication3/Models/Otel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Otel
+    {
+        public int Id { get; set; }
+        public string Adı { get; set; }
+        public string Aciklama { get; set; }
+        public string Turu { get; set; }
+        public int Puan { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public int Telefon { get; set; }
+    }
+}
